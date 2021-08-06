c4ee193bec3346434f96ccad0e077e5bcdfbd099
bamartinezd <alejandromardelg@gmail.com>
2021-08-06 17:48:37 -0500

    experience card

---
+++ data/card-experience.data.js
@@ -0,0 +1,38 @@
+const experience = [{
+        image: 'https://brand.globant.com/assets/images/globant-lightbg-color-2.png',
+        position: '.Net Developer',
+        companyName: 'Globant',
+        dateRange: 'May 19, 2021 - Current',
+        website: 'https://www.globant.com/'
+    },
+    {
+        image: 'https://gmlsoftware.com/wp-content/uploads/2021/01/logo__contact__section.png',
+        position: 'Consultant/.Net Developer',
+        companyName: 'GML Software',
+        dateRange: 'May 19, 2021 - Current',
+        website: 'https://gmlsoftware.com/'
+    },
+    {
+        image: 'https://media-exp1.licdn.com/dms/image/C560BAQFOQ05GxvBxFw/company-logo_200_200/0?e=2159024400&v=beta&t=qKsVKg6sdAyNP-Esoq7KT51r6yL23BEJ0d1wjG_gATw',
+        position: 'Consultant/.Net Developer',
+        companyName: 'Periferia IT Group',
+        dateRange: 'May 19, 2021 - Current',
+        website: 'https://periferiaitgroup.com/'
+    },
+    {
+        image: 'https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/b5/e2/56/b5e256d5-97ef-a9e4-eb73-bc85d904b021/source/512x512bb.jpg',
+        position: '.Net Developer',
+        companyName: 'Global Hitss',
+        dateRange: 'May 19, 2021 - Current',
+        website: 'https://globalhitss.com/'
+    },
+    {
+        image: 'https://infolocal.comfenalcoantioquia.com/media/com_jbusinessdirectory/pictures/companies/679/saludtotal-1496934398.jpg',
+        position: 'Programmer 1',
+        companyName: 'Salud Total EPS',
+        dateRange: 'May 19, 2021 - Current',
+        website: 'https://saludtotal.com.co/'
+    }
+];
+
+export default experience;
--- data/card-skill.data.js
+++ data/card-skill.data.js
@@ -1,25 +1,31 @@
 const skills = [{
         image: 'https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg',
-        text: 'CSS 3'
+        text: 'CSS 3',
+        website: 'https://www.w3.org/Style/CSS/Overview.en.html'
     },
     {
         image: "https://hackr.io/tutorials/learn-html-5/logo/logo-html-5?ver=1587977020",
         text: "HTML 5",
+        website: 'https://html5.org/'
     },
     {
         image: "https://miro.medium.com/max/1280/1*oI32P7xHwEsd2TaV6_GUWA.jpeg",
         text: "Angular",
+        website: 'https://angular.io/'
     },
     {
         image: "https://res.cloudinary.com/teepublic/image/private/s--HGX34F_2--/t_Preview/b_rgb:ffb81c,c_lpad,f_jpg,h_630,q_90,w_1200/v1539273850/production/designs/3302095_0.jpg",
         text: "JS",
+        website: 'https://www.javascript.com/'
     },
     {
         image: "https://appdatos.com/wp-content/uploads/2019/12/csharp_api.jpg",
         text: "C#",
+        website: 'https://dotnet.microsoft.com/'
     }, {
         image: "https://adaptivedge.com/wp-content/uploads/2018/10/SQL-Server-Logo.jpg",
         text: "SQL",
+        website: 'https://www.microsoft.com/en-us/sql-server'
     }
 ];
 
