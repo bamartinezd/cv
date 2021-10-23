129f7dcb068a2010f85c84db182d42301a2e3f91
bamartinezd <alejandromardelg@gmail.com>
2021-10-22 19:33:28 -0500

    Updating CV pdf and fixing data of experiences dates

--- data/card-experience.data.js
+++ data/card-experience.data.js
@@ -9,28 +9,28 @@ const experience = [{
         image: 'assets/gml-logo.jpg',
         position: 'Consultant/.Net Developer',
         companyName: 'GML Software',
-        dateRange: 'May 19, 2021 - Current',
+        dateRange: 'July 2019 - May 2021',
         website: 'https://gmlsoftware.com/'
     },
     {
         image: 'https://media-exp1.licdn.com/dms/image/C560BAQFOQ05GxvBxFw/company-logo_200_200/0?e=2159024400&v=beta&t=qKsVKg6sdAyNP-Esoq7KT51r6yL23BEJ0d1wjG_gATw',
         position: 'Consultant/.Net Developer',
         companyName: 'Periferia IT Group',
-        dateRange: 'May 19, 2021 - Current',
+        dateRange: 'September 2018 - April 2019',
         website: 'https://periferiaitgroup.com/'
     },
     {
         image: 'https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/b5/e2/56/b5e256d5-97ef-a9e4-eb73-bc85d904b021/source/512x512bb.jpg',
         position: '.Net Developer',
         companyName: 'Global Hitss',
-        dateRange: 'May 19, 2021 - Current',
+        dateRange: 'October 2017 - June 2018',
         website: 'https://globalhitss.com/'
     },
     {
         image: 'https://infolocal.comfenalcoantioquia.com/media/com_jbusinessdirectory/pictures/companies/679/saludtotal-1496934398.jpg',
         position: 'Programmer 1',
         companyName: 'Salud Total EPS',
-        dateRange: 'May 19, 2021 - Current',
+        dateRange: 'January 2016 - May 2017',
         website: 'https://saludtotal.com.co/'
     }
 ];
