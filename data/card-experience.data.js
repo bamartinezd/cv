0e6a67b4d72b63dffd5623c16ca871317c09179e
Alejandro Martinez <alejandromardelg@gmail.com>
2022-01-03 17:27:53 -0500

    Updating experience companies to local images

--- data/card-experience.data.js
+++ data/card-experience.data.js
@@ -1,5 +1,5 @@
 const experience = [{
-        image: 'https://brand.globant.com/assets/images/globant-lightbg-color-2.png',
+        image: 'assets/Short-Original.jpg',
         position: '.Net Developer',
         companyName: 'Globant',
         dateRange: 'May 19, 2021 - Current',
@@ -13,21 +13,21 @@ const experience = [{
         website: 'https://gmlsoftware.com/'
     },
     {
-        image: 'https://media-exp1.licdn.com/dms/image/C560BAQFOQ05GxvBxFw/company-logo_200_200/0?e=2159024400&v=beta&t=qKsVKg6sdAyNP-Esoq7KT51r6yL23BEJ0d1wjG_gATw',
+        image: 'assets/PeriferiaLogo.jpg',
         position: 'Consultant/.Net Developer',
         companyName: 'Periferia IT Group',
         dateRange: 'September 2018 - April 2019',
         website: 'https://periferiaitgroup.com/'
     },
     {
-        image: 'https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/b5/e2/56/b5e256d5-97ef-a9e4-eb73-bc85d904b021/source/512x512bb.jpg',
+        image: 'assets/Hitss.jpg',
         position: '.Net Developer',
         companyName: 'Global Hitss',
         dateRange: 'October 2017 - June 2018',
         website: 'https://globalhitss.com/'
     },
     {
-        image: 'https://infolocal.comfenalcoantioquia.com/media/com_jbusinessdirectory/pictures/companies/679/saludtotal-1496934398.jpg',
+        image: 'assets/saludtotal-1496934398.jpg',
         position: 'Programmer 1',
         companyName: 'Salud Total EPS',
         dateRange: 'January 2016 - May 2017',
